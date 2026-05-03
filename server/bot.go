50de999e7b165f41bc4271b2f7296d8efd3a8e9c
Artem Zakirullin <artemzr@gmail.com>
2026-05-03 10:55:47 +0300

    fix better welcome

--- server/bot.go
+++ server/bot.go
@@ -1675,7 +1675,7 @@ func (b *Bot) showStart(params []string) error {
 		}
 	}
 
-	return b.showHTML("Welcome!\n\nYou can send me any stuff and I'll save it to files!\n\nBy default <code>Full Mode</code> is enabled, it can feel overwhelming. You can switch to notes only or tasks only mode in /settings command.", nil)
+	return b.showHTML("Welcome! 👋\n\nSend me anything, and I’ll save it to files!\n\nBy default <code>Full Mode</code> is enabled, which can feel a bit overwhelming. You can switch to <b>notes only</b> or <b>tasks only</b> mode in /settings menu.", nil)
 }
 
 func (b *Bot) moveToDirFromToday(params []string) error {
